acf8795f2b74b0d7b5a3ebc8a9b3a01281cfecb0
Nour Benhassen <nour.benhassen@gmail.com>
2019-07-01 12:16:08 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: '586997336706-qmighjl1e1tl9iiodtfhltatiddqu5ag.apps.googleusercontent.com',
+    googleClientSecret: 'yAsKqkeMNpcqKTEeEnWsA7vF',
+    mongoURI: 'mongodb+srv://nourDB:zest2992@emaily-s0y41.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'blabla'
+};
+
+
+//mongodb+srv://dbUser:nour2992@cluster0-pk3up.azure.mongodb.net/test?retryWrites=true&w=majority
+
