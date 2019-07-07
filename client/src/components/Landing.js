6f440efc00d55f1b149049bbca2a21c97fe0b753
Nour <nour.benhassen@gmail.com>
2019-07-08 00:20:03 +0200

    Front + Back in place

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { connect } from 'react-redux'
+
+class Landing extends React.Component {
+    render (){
+    return (
+        <div>
+        Collect feedback from your users
+        </div>
+    )
+}
+}
+
+
+export default Landing
--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,18 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true 
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleID: profile.id}).then((existingUser) => {
-                if (existingUser) {
+    async (accessToken, refreshToken, profile, done) => {
 
-                    done(null, existingUser);
-                } else {
-                    new User({ googleID: profile.id }).save()
-                    .then(user => done(null,user));
-
-                }
-            })
-
-    }
-  )
+       const existingUser =  await User.findOne({ googleID: profile.id})
+        
+        if (existingUser) {
+            // we already have a record with the given profile ID
+            done(null, existingUser);
+        } else {
+            //we don't have a user record with this ID, make a new record
+          const user = await new User({ googleID: profile.id }).save()
+          done(null, user);
+         }
+        }
+    )
 );
